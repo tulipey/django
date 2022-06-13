6875ed6aa4c3848353c999b3bc5de4c9700d4664
etudiant <etudiant@deb11-vdi-info>
2022-06-13 23:29:52 +0200

    base de donnée modifier

--- django/TD1/TD1App/models.py
+++ django/TD1/TD1App/models.py
@@ -1,3 +1,4 @@
+from typing import Type
 from django.db import models
 from datetime import datetime
 
@@ -26,3 +27,37 @@ class Employe (models.Model):
     nom = models.CharField (max_length=35)
     Date_DE_NAISSANCE = models.DateField (default = datetime.now())
     mach = models.CharField (max_length=32, choices=TYPE, default='Technicien')
+
+class Compte (models.Model):
+
+    TYPE = (
+        ('Technicien', ('Tecnicien')),
+        ('User', ('Utilisateur')),
+    )
+
+    id_c = models.AutoField (primary_key=True, editable=False)
+    nom = models.CharField (max_length=17)
+    prenom = models.CharField (max_length=20)
+    password = models.CharField (max_length=20)
+    poste = models.CharField (max_length=15, choices=TYPE, default='User')
+    email = models.EmailField (max_length=50)
+
+class Infrastructure (models.Model):
+
+    id_i = models.AutoField (primary_key=True, editable=False)
+    entreprise = models.CharField (max_length=30)
+    responsable = models.CharField (max_length=17)
+
+class Equipement (models.Model):
+
+    TYPE = (
+        ('Ordinateur'), ('Ordinateur'),
+        ('Switch'), ('Commutateur'),
+        ('Routeur'), ('Router'),
+    )
+
+    id_e = models.AutoField (primary_key=True, editable=False)
+    Type = models.CharField (max_length=15, choices=TYPE, default='Ordinateur')
+    infra = models.CharField (max_length=30)
+    user = models.CharField (max_length=17)
+    maj = models.DateField (default = datetime.now())
